73b59db59512e2c00304be815ca8665fefdd3a57
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-01-06 13:06:25 +0200

    section4 -ready

--- src/components/Layout/Main/MainSection4.tsx
+++ src/components/Layout/Main/MainSection4.tsx
@@ -49,7 +49,33 @@ const useStyles = makeStyles((theme: Theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
   link: {
-    color: theme.palette.primary.light,
+    width: '100%',
+    textAlign: 'right',
+    marginTop: '20px',
+    paddingRight: '8%',
+  },
+  imageItem: {
+    height: '85px',
+    maxWidth: '210px',
+    margin: '25px 40px 0 0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    border: '1px solid #e5e5e5',
+    '&:hover': {
+      borderColor: 'transparent',
+      boxShadow: '0 0 6px rgba(81, 112, 136, .35)',
+    },
+    '@media (max-width: 991px)': {
+      margin: '10px 10px',
+    },
+  },
+  image: {
+    display: 'block',
+    padding: 0,
+    maxHeight: '56px',
   },
 }))
 
@@ -77,16 +103,445 @@ export default function TabsWrappedLabelSection4() {
             <Tab value="three" label="CRYPTO" {...a11yProps('three')} />
           </Tabs>
         </AppBar>
+        {/* 1-st Panel*/}
         <TabPanel value={value} index="one">
-          <Box></Box>
+          <Grid container>
+            {/* First line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/dxfeed.png"
+                  alt="dxFeed"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/rithmic.svg"
+                  alt="Rithmic"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/trading-tech.svg"
+                  alt="Trading Technology"
+                  width={142}
+                  height={41}
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/cqg.svg"
+                  alt="CQG"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            {/* 2 line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/gain-capital.svg"
+                  alt="Gain Capital"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/ninja-traders.svg"
+                  alt="Ninja Traders"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/transact.svg"
+                  alt="TransAct Futures"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/iqfeed.svg"
+                  alt="iQFEED"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            {/* 3 line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/stage.svg"
+                  alt="Stage Five"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/cedro.png"
+                  alt="Cedro Technologies"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/eurex.png"
+                  alt="EUREX"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/edge-clear.png"
+                  alt="Edge Clear"
+                  width={120}
+                  height={100}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+          </Grid>
         </TabPanel>
+        {/* 2-nd Panel*/}
         <TabPanel value={value} index="two">
-          <Box></Box>
+          <Grid container>
+            {/* First line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/dxfeed.png"
+                  alt="dxFeed"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/2/ts.jpg"
+                  alt="Trade Station"
+                  width={142}
+                  height={80}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/2/ib.jpg"
+                  alt="Interactive brokers"
+                  width={142}
+                  height={80}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/1/cedro.png"
+                  alt="CEDRO Technologies"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+          </Grid>
         </TabPanel>
+        {/* 3-d Panel*/}
         <TabPanel value={value} index="three">
-          <Box></Box>
+          <Grid container>
+            {/* First line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/binance.png"
+                  alt="Binance"
+                  width={142}
+                  height={35}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/binance-futures.png"
+                  alt="Binance Futures"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/bitmex.png"
+                  alt="BitMEX"
+                  width={142}
+                  height={25}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/bitflyer.png"
+                  alt="BitFlyer"
+                  width={142}
+                  height={65}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            {/* 2 line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/bitstamp.png"
+                  alt="BitStamp"
+                  width={130}
+                  height={90}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/btse.png"
+                  alt="BTSE"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/bittrex.png"
+                  alt="BITTREX"
+                  width={142}
+                  height={30}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/bybit.png"
+                  alt="bybit"
+                  width={110}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            {/* 3 line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/bitfinex.svg"
+                  alt="BITFINEX"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/coinbase.png"
+                  alt="Coinbase"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/coinflex.png"
+                  alt="CoinFLEX"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/deribit.png"
+                  alt="Deribit"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            {/* 4 line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/ftx.png"
+                  alt="FTX"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/hitbtc.jpg"
+                  alt="HitBTC"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/huobi.png"
+                  alt="Huobi"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/krakenf.jpg"
+                  alt="KRAKEN Futures"
+                  width={142}
+                  height={16}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            {/* 5 line*/}
+            <Grid md={3} xs={6} alignItems="center">
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/kraken.png"
+                  alt="KRAKEN"
+                  width={142}
+                  height={20}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/liquid.png"
+                  alt="Liquid"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/okex.png"
+                  alt="OKEX"
+                  width={142}
+                  height={41}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/okcoin.svg"
+                  alt="OKCOIN"
+                  width={142}
+                  height={20}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Box className={classes.imageItem}>
+                <Image
+                  src="/static/main/s4/3/poloniex.png"
+                  alt="POLONIEX"
+                  width={142}
+                  height={80}
+                  layout="intrinsic"
+                />
+              </Box>
+            </Grid>
+          </Grid>
         </TabPanel>
       </div>
+      <Box className={classes.link}>
+        <Link
+          href="https://www.bookmap.com/knowledgebase/docs/KB-IntroductionToBookmap-Connectivity"
+          color="secondary"
+        >
+          Learn more
+        </Link>
+      </Box>
     </Container>
   )
 }
