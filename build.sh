126a3c0023e50dfb8ffaab5c38b7cbcafb4699dc
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-01-18 12:29:04 +0200

    prepare deployment

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ echo "npm install"
 npm install && NODE_ENV=production npm run build
 #echo "Build app"
 #npx next build
+echo "delete app next"
+pm2 delete next
 echo "starting app as daemon"
-pm2 start npm --name "my_app_name" -- start
+pm2 start npm --name "next" -- start
 #pm2 start npm --name "next" -- run dev
 #pm2 restart next
