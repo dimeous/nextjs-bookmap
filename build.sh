475ecade6cdaf9b571d8f4e9ac773b9f5e7ea827
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-01-22 07:13:24 +0200

    gitlab2

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ npm install && NODE_ENV=production npm run build
 echo "restart app next"
 pm2 restart bookmap-site
 #echo "starting app as daemon"
-#pm2 start npm --name "bookmap-site" -- start
+#pm2 start npm --name "bookmap" -- start
 #pm2 start npm --name "next" -- run dev
 #pm2 restart next
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,23 @@ module.exports = {
             script: 'npm',
             args: 'start',
             cwd: '/home/gitlab-runner/builds/7G-8ML1s/0/web-dev/landings/',
+            env: {
+                NODE_ENV: 'development'
+            },
             env_production: {
                 NODE_ENV: 'production',
+                PORT: 3000
             }
         },
+
 // optionally a second project
     ],
+    deploy: {
+        production: {
+            'post-deploy': 'npm install --production'
+                + ' && pm2 startOrRestart ecosystem.config.js --env=production'
+                + ' && pm2 save'
+        }
+    }
+
 };
