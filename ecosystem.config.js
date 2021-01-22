0c84df9a5f9e7ffadb02484ae173a03b502d4bfe
highlander <arshinchenko@gmail.com>
2021-01-22 05:43:39 +0200

    gitlab test3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,8 @@ module.exports = {
             script: 'npm',
             args: 'start',
             cwd: '/home/gitlab-runner/builds/7G-8ML1s/0/web-dev/landings/',
-            env: {
-                NODE_ENV: 'development'
-            },
             env_production: {
                 NODE_ENV: 'production',
-                PORT: 3000,
             }
         },
 // optionally a second project
