6b5c3f1c2663279128c63c91ee779a83077a612a
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-01-22 08:50:14 +0200

    gitlab3

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "Building and deploying app\n"
-git pull
 echo "npm install and build"
 npm install && NODE_ENV=production npm run build
-echo "restart app next"
-pm2 restart bookmap-site
+echo "stop app "
+pm2 stop bookmap 2>/dev/null
+echo "delete app "
+pm2 delete bookmap 2>/dev/null
+echo "start app next"
+pm2 start npm --name "bookmap" -- start
+pm2 save
 #echo "starting app as daemon"
 #pm2 start npm --name "bookmap" -- start
 #pm2 start npm --name "next" -- run dev
