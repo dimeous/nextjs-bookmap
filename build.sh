5f870530ed067c78c32b27f4cb3bacaec9bca400
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-01-28 15:10:00 +0200

    crypto section 2

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "Building and deploying app\n"
-echo "npm install and build"
-npm install
-#npm install && NODE_ENV=production npm run build
-echo "npm build"
-npm run build
+
 echo "stop app "
 pm2 stop bookmap 2>/dev/null
 echo "delete app "
 pm2 delete bookmap 2>/dev/null
+
+echo "npm install and build"
+#npm install
+npm install && NODE_ENV=production npm run build
+#echo "npm build"
+#npm run build
+
 echo "start app next"
 pm2 start npm --name "bookmap" -- start
 pm2 save
