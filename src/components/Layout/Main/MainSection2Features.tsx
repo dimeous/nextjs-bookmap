daf4eb66416a688430a1adad808189706f322bc1
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-03-16 11:29:12 +0200

    flipping  tabs on main

--- src/components/Layout/Main/MainSection2Features.tsx
+++ src/components/Layout/Main/MainSection2Features.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 import Tabs from '@material-ui/core/Tabs'
 import Link from '@material-ui/core/Link'
@@ -71,11 +71,36 @@ const useStyles = makeStyles((theme: Theme) => ({
 }))
 
 export default function MainSection2Features() {
+  const flippingTime = 5000
   const classes = useStyles()
+  const [clicked, setClicked] = React.useState(0)
   const [value, setValue] = React.useState('one')
+  const nextSlide = () => {
+    if (clicked) return
+    if (value == 'one') {
+      setValue('two')
+      return
+    }
+    if (value == 'two') {
+      setValue('three')
+      return
+    }
+    if (value == 'three') {
+      setValue('one')
+      return
+    }
+  }
+
+  useEffect(() => {
+    const interval_id = setInterval(nextSlide, flippingTime)
+    return () => {
+      clearInterval(interval_id)
+    }
+  })
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
     setValue(newValue)
+    setClicked(1)
   }
 
   return (
