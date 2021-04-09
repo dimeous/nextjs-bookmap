4f6395360b89eb5db914459cd084c50a2bb3f757
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-04-09 11:49:11 +0300

    webpack5

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@ const withFonts = require('next-fonts')
 
 module.exports = withImages(withFonts({
     webpack(config) {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            // your aliases
+        };
+
+        // config.node = {
+        //   fs: "empty",
+        //   net: "empty",
+        //   tls: "empty",
+        //   child_process: "empty",
+        //   console: true,
+        // };
+        // Replaced above with below for webpack 5.
+        // Reference: https://webpack.js.org/migrate/5/#clean-up-configuration
+
         return config;
-    }
+    },
+    poweredByHeader: false,
+    future: {
+        webpack5: true,
+    },
 }))
