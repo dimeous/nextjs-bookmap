d7cce805e38c2e79cd13de012bdffc080b400c1a
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2021-07-05 13:26:20 +0300

    add general utm

--- src/components/Layout/Tradovate/TradSection2.tsx
+++ src/components/Layout/Tradovate/TradSection2.tsx
@@ -1,4 +1,13 @@
-import { List, ListItem, ListItemAvatar, ListItemText, Typography, Avatar, Box, Grid } from '@material-ui/core'
+import {
+  Avatar,
+  Box,
+  Grid,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from '@material-ui/core'
 import Container from '@material-ui/core/Container'
 import { Theme } from '@material-ui/core/styles'
 import { makeStyles } from '@material-ui/styles'
@@ -48,14 +57,14 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
   },
   icon: {
-      height: "65px",
-      width: "65px",
-      borderRadius: "5px",
-      fontSize: "28px"
+    height: '65px',
+    width: '65px',
+    borderRadius: '5px',
+    fontSize: '28px',
   },
   listItemText: {
-    color: "#124256"
-  }
+    color: '#124256',
+  },
 }))
 
 type ElementProperties = {
@@ -63,72 +72,118 @@ type ElementProperties = {
 }
 
 const TradSection2 = ({ page }: ElementProperties): React.ReactElement => {
-
-
   const classes = useStyles()
   return (
     <Container fixed>
-        <Container className={classes.textBlock}>
-            <Grid item md={5}>
-                <Typography component="h2" variant="h2" color="inherit" gutterBottom>
-                    Bookmap and Tradovate offer trading on FairX with
-                </Typography>
-            </Grid>
-        </Container>
-        <Grid container>
-            <Grid item xs={12} md={5}>
-                <List dense className={classes.root}>
-                    <ListItem className={classes.listitem}>
-                        <ListItemAvatar>
-                            <Avatar variant="square" style={{backgroundColor: "#F9A400"}} className={classes.icon}>
-                                <span style={{fontSize: "13px", marginTop: "5px"}}>$</span>0
-                            </Avatar>
-                        </ListItemAvatar>
-                        <Box ml={10} display="flex" flexDirection="column">
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="ZERO" className={classes.listItemText} />
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="Exchange fees" className={classes.listItemText} />
-                        </Box>
-                    </ListItem>
-                    <ListItem className={classes.listitem}>
-                        <ListItemAvatar>
-                            <Avatar variant="square" style={{backgroundColor: "#607D8B"}} className={classes.icon}>
-                                <span style={{fontSize: "13px", marginTop: "5px"}}>$</span>0
-                            </Avatar>
-                        </ListItemAvatar>
-                        <Box ml={10} display="flex" flexDirection="column">
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="ZERO" className={classes.listItemText} />
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="Broker commissions" className={classes.listItemText} />
-                        </Box>
-                    </ListItem>
-                </List>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <List dense className={classes.root}>
-                    <ListItem className={classes.listitem}>
-                        <ListItemAvatar>
-                            <Avatar variant="square" style={{backgroundColor: "#10DB61"}} className={classes.icon}>
-                                <span style={{fontSize: "13px", marginTop: "5px"}}>$</span>0
-                            </Avatar>
-                        </ListItemAvatar>
-                        <Box ml={10} display="flex" flexDirection="column">
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="ZERO" className={classes.listItemText} />
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="Market data cost" className={classes.listItemText} />
-                        </Box>
-                    </ListItem>
-                    <ListItem className={classes.listitem}>
-                        <ListItemAvatar>
-                            <Avatar variant="square" style={{backgroundColor: "#0085F9"}} className={classes.icon}>
-                                <span style={{fontSize: "13px", marginTop: "5px"}}>$</span>0
-                            </Avatar>
-                        </ListItemAvatar>
-                        <Box ml={10} display="flex" flexDirection="column">
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="ZERO" className={classes.listItemText} />
-                            <ListItemText id="checkbox-list-secondary-label-1" primary="Bookmap subscription fees" className={classes.listItemText} />
-                        </Box>
-                    </ListItem>
-                </List>
-            </Grid>
+      <Container className={classes.textBlock}>
+        <Grid item md={5}>
+          <Typography component="h2" variant="h2" color="inherit" gutterBottom>
+            Bookmap and Tradovate offer trading on FairX with
+          </Typography>
         </Grid>
+      </Container>
+      <Grid container>
+        <Grid item xs={12} md={5}>
+          <List dense className={classes.root}>
+            <ListItem className={classes.listitem}>
+              <ListItemAvatar>
+                <Avatar
+                  variant="square"
+                  style={{ backgroundColor: '#F9A400' }}
+                  className={classes.icon}
+                >
+                  <span style={{ fontSize: '13px', marginTop: '5px' }}>$</span>0
+                </Avatar>
+              </ListItemAvatar>
+              <Box ml={10} display="flex" flexDirection="column">
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="ZERO"
+                  className={classes.listItemText}
+                />
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="Exchange fees"
+                  className={classes.listItemText}
+                />
+              </Box>
+            </ListItem>
+            <ListItem className={classes.listitem}>
+              <ListItemAvatar>
+                <Avatar
+                  variant="square"
+                  style={{ backgroundColor: '#607D8B' }}
+                  className={classes.icon}
+                >
+                  <span style={{ fontSize: '13px', marginTop: '5px' }}>$</span>0
+                </Avatar>
+              </ListItemAvatar>
+              <Box ml={10} display="flex" flexDirection="column">
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="ZERO"
+                  className={classes.listItemText}
+                />
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="Broker commissions"
+                  className={classes.listItemText}
+                />
+              </Box>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <List dense className={classes.root}>
+            <ListItem className={classes.listitem}>
+              <ListItemAvatar>
+                <Avatar
+                  variant="square"
+                  style={{ backgroundColor: '#10DB61' }}
+                  className={classes.icon}
+                >
+                  <span style={{ fontSize: '13px', marginTop: '5px' }}>$</span>0
+                </Avatar>
+              </ListItemAvatar>
+              <Box ml={10} display="flex" flexDirection="column">
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="ZERO"
+                  className={classes.listItemText}
+                />
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="Market data cost"
+                  className={classes.listItemText}
+                />
+              </Box>
+            </ListItem>
+            <ListItem className={classes.listitem}>
+              <ListItemAvatar>
+                <Avatar
+                  variant="square"
+                  style={{ backgroundColor: '#0085F9' }}
+                  className={classes.icon}
+                >
+                  <span style={{ fontSize: '13px', marginTop: '5px' }}>$</span>0
+                </Avatar>
+              </ListItemAvatar>
+              <Box ml={10} display="flex" flexDirection="column">
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="ZERO"
+                  className={classes.listItemText}
+                />
+                <ListItemText
+                  id="checkbox-list-secondary-label-1"
+                  primary="Bookmap subscription fees"
+                  className={classes.listItemText}
+                />
+              </Box>
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
